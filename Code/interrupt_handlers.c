ab36bdb047f044d99ccc10b3235c86cf709d4d60
Penjo989 <eyalangel0@gmail.com>
2024-09-19 15:38:56 +0300

    made simple program that turns on all 4 leds on reset

--- Code/interrupt_handlers.c
+++ Code/interrupt_handlers.c
@@ -1,19 +1,27 @@
+/* Simple program to lit up all 4 leds on reset*/
 
+#define RCC_BASE (0x40023800)
+#define RCC_AHB1ENR (RCC_BASE + 0x30)
+#define GPIOD_BASE (0x40020C00)
+#define GPIOD_MODER (GPIOD_BASE)
+#define GPIOD_BSRR (GPIOD_BASE + 0x18)
 
 void Reset_Handler()
 {
-    _start:
-    int a = 0;
-    a += 1;
-    Reset_Handler();
+    // turn on gpiod clock enable on rcc
+    int *rcc_ahb1enr = RCC_AHB1ENR;
+    *rcc_ahb1enr |= 0xf;
+
+    // configure gpiod pins moder
+    unsigned long int *gpiod_moder = GPIOD_MODER;
+    *gpiod_moder = 0x55555555;
+    
+    // turn on leds
+    int *gpiod_bsrr = GPIOD_BSRR;
+    *gpiod_bsrr = 0xffff;
+    while (1) { }
 }
 
 void Default_Handler()
 {
-    Default_Handler();
-}
-
-int main()
-{
-    return;
 }
