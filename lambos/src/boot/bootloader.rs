4e2117cb250f0d52a6181e035cde396bdad2f6d7
Yoav Sakal <yoavarviv1@gmail.com>
2024-10-17 17:52:14 +0300

    need to fix copying

--- lambos/src/boot/bootloader.rs
+++ lambos/src/boot/bootloader.rs
@@ -1,10 +1,29 @@
 use cortex_m_semihosting::hprintln;
 
-pub fn boot() -> () {
+extern "C" {
+    static __sdata: u32;
+}
+
+pub extern "C" fn boot() -> () {
     hprintln!("Bootloader running... {:p}", lambos_reset as *const()).unwrap();
+    unsafe {
+        let data_start = __sdata as *mut u8;
+        let reset_ptr = lambos_reset as *const u8;
+        let reset_size: u16 = 0x4a;
+        
+        hprintln!("Starting to copy reset to RAM... {:p} {:p}", reset_ptr, data_start).unwrap();
+
+        for i in 0..reset_size {
+            *data_start.add(i.into()) = *reset_ptr.add(i.into())
+        }
+
+        hprintln!("Done copying...").unwrap();
+    }
     lambos_reset();
 }
 
+#[link_section = ".data"]
+#[inline(never)]
 fn lambos_reset() -> () {
     hprintln!("Resetting lambos...").unwrap();
 }
--- lambos/src/main.rs
+++ lambos/src/main.rs
@@ -5,6 +5,7 @@ use panic_halt as _;
 use cortex_m::asm;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
+use stm32f4xx_hal::{};
 
 mod boot;
 use boot::bootloader;
