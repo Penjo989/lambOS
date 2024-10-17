edb2274897c212baacf68be211aa714f59c017ec
Yoav Sakal <yoavarviv1@gmail.com>
2024-10-18 01:29:46 +0300

    working bootloader

--- lambos/src/boot/bootloader.rs
+++ lambos/src/boot/bootloader.rs
@@ -1,29 +1,49 @@
 use cortex_m_semihosting::hprintln;
+use cortex_m_semihosting::hio;
+
+use core::ptr;
+use core::ptr::addr_of; 
+
+const BOOT_OFFSET: u32 = 0x1000;
 
 extern "C" {
-    static __sdata: u32;
+    static mut __sdata: u8;
+    static mut _sboot: u8;
+    static mut _eboot: u8;
 }
 
 pub extern "C" fn boot() -> () {
-    hprintln!("Bootloader running... {:p}", lambos_reset as *const()).unwrap();
+    hprintln!("Bootloader running...").unwrap();
     unsafe {
-        let data_start = __sdata as *mut u8;
-        let reset_ptr = lambos_reset as *const u8;
-        let reset_size: u16 = 0x4a;
-        
-        hprintln!("Starting to copy reset to RAM... {:p} {:p}", reset_ptr, data_start).unwrap();
+        let code_start: u32 = addr_of!(__sdata) as *const u8 as u32 + BOOT_OFFSET;
+        hprintln!("Code will start @ {:x}", code_start).unwrap();
 
+        let data_start = code_start as *const u32 as *mut  u8;  
+        let reset_ptr = (lambos_reset as *const u8).sub(1);
+        let reset_size: u16 = (addr_of!(_eboot) as *const u8 as u16) - (addr_of!(_sboot) as *const u8 as u16);
+        hprintln!("Size of function @ {:x}", reset_size).unwrap();
+        
         for i in 0..reset_size {
-            *data_start.add(i.into()) = *reset_ptr.add(i.into())
+            ptr::write(data_start.add(i.into()), reset_ptr.add(i.into()).read());
         }
 
-        hprintln!("Done copying...").unwrap();
+        hprintln!("Jumping to RAM reset...").unwrap();
+
+        let reset_func: fn(fn(&str)) = core::mem::transmute(code_start | 1);
+        reset_func(semihosting_print);
     }
-    lambos_reset();
 }
 
-#[link_section = ".data"]
+pub fn semihosting_print(message: &str) {
+    if let Ok(mut hstdout) = hio::hstdout() {
+        use core::fmt::Write;
+        writeln!(hstdout, "{}", message).ok();  // Ignore the Result and force it to return ()
+    }
+}
+
+#[link_section = ".boot"]
 #[inline(never)]
-fn lambos_reset() -> () {
-    hprintln!("Resetting lambos...").unwrap();
+#[no_mangle]
+unsafe fn lambos_reset(hp: fn(&str)) -> () {
+    hp("Resetting lambos...");
 }
